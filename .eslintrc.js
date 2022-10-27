5d8490f8faef7b4b247256efef9fe485191ba382
taesung1993 <cheonyulin@gmail.com>
2022-10-27 14:27:24 +0900

    feat: setting prettier and eslint in husky

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'comma-dangle': ['error', 'never'],
+    'object-curly-newline': 'off'
+  }
+};
