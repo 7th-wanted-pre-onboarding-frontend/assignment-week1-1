af681769425205c0def3cdb7b7965224a0361101
task11 <task11@naver.com>
2022-10-28 00:31:59 +0900

    fix: axios interceptor token 처리 로직 중복 제거

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,8 +1,7 @@
-
 import React, { useState, useEffect } from 'react';
-import TodoItem from '../Components/TodoItem';
 import TodoService from '../utils/Todo.service';
 import ProtectedRoute from '../ProtectedRouter';
+import TodoList from '../Components/TodoList';
 
 function Todo() {
   const [todo, setTodo] = useState('');
@@ -29,38 +28,34 @@ function Todo() {
     }
   };
 
-  useEffect(() => {
-    const getTodolist = async () => {
-      const { data } = await TodoService.get();
-      setTodoItemArry(data);
-    };
+  const getTodoList = async () => {
+    const { data } = await TodoService.get();
+    setTodoItemArry(data);
+  };
 
+  useEffect(() => {
     try {
-      getTodolist();
+      getTodoList();
     } catch (e) {
       alert('문제가 발생했습니다.');
     }
   }, []);
 
   return (
-  <ProtectedRoute>
-    <form onSubmit={onSubmit}>
-      <div>
-        <input
-          type="text"
-          placeholder="할 일을 입력하세요."
-          onChange={handleInput}
-          value={todo}
-        />
-        <button type="submit">생성</button>
-      </div>
-      <div>
-        {todoItemArray.map(({ id }) => (
-          <TodoItem key={id} />
-        ))}
-      </div>
-    </form>
-  </ProtectedRoute>
+    <ProtectedRoute>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="할 일을 입력하세요."
+            onChange={handleInput}
+            value={todo}
+          />
+          <button type="submit">생성</button>
+        </div>
+        <TodoList todos={todoItemArray} getTodoList={getTodoList} />
+      </form>
+    </ProtectedRoute>
   );
 }
 
--- src/utils/Todo.service.js
+++ src/utils/Todo.service.js
@@ -1,43 +1,34 @@
 import client from './CustomAxios';
-import LocalStorageService from './LocalStorage.service';
 
 class TodosService {
   create(todo) {
-    const { value: accessToken } = LocalStorageService.get('token');
     const body = { todo };
     return client.post('todos', body, {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
       }
     });
   }
 
   get() {
-    const { value: accessToken } = LocalStorageService.get('token');
     return client.get('todos', {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
       }
     });
   }
 
   update(id, payload) {
-    const { value: accessToken } = LocalStorageService.get('token');
     return client.put(`todos/${id}`, payload, {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
       }
     });
   }
 
   delete(id) {
-    const { value: accessToken } = LocalStorageService.get('token');
     return client.delete(`todos/${id}`, {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
       }
     });
